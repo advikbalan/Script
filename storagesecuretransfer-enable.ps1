63511ba2f6b373e2bc6408680c58d093483d2f21
advikbalan <86916015+advikbalan@users.noreply.github.com>
2021-09-30 12:55:18 +0530

    Secure Transfer - Storage Account script

---
+++ storagesecuretransfer-enable.ps1
@@ -0,0 +1,11 @@
+﻿$Storagename = @("testsecureaccount","aicvsstorageaccount")
+$ResourceGroupName = @("cvs_lab_1","cvs_lab_2")
+$outputfilepath = 'C:\Code\File.txt'
+
+for ($i=0; $i-lt $Storagename.Length; $i++)
+	{
+	Set-AzStorageAccount -ResourceGroupName $ResourceGroupName[$i] -Name $Storagename[$i] -EnableHttpsTrafficOnly $True
+	get-AzStorageAccount -ResourceGroupName $ResourceGroupName[$i] -Name $Storagename[$i] | Out-File -FilePath $outputfilepath -Append
+	}
+
+
