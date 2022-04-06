0cde9c3bd15a1a3038d0b78f613c1a3c56da4f99
advikbalan <86916015+advikbalan@users.noreply.github.com>
2022-04-06 16:45:49 +0530

    Update DiskReport.ps1

--- DiskReport.ps1
+++ DiskReport.ps1
@@ -1,4 +1,4 @@
 $Disk=Get-AzDisk
-$Orphan=$Disk | Select-Object -Property Name,ResourceGroupName,Type,DiskSizeGB,DiskState
+$Orphan=$Disk | Select-Object -Property @{label='Sub';expression={$_.id.Substring(15,36)}},Name,ResourceGroupName,Type,DiskSizeGB,DiskState
 $a=$Orphan | Where-Object -Property DiskSizeGB -ge “1024”
 $a | Export-Csv -Path c:\temp\DiskReport.csv -NoTypeInformation -Force
