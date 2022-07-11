204447e9f7789e5acc1991b998e314b5a6d8d6f3
advikbalan <86916015+advikbalan@users.noreply.github.com>
2022-07-11 19:28:21 +0530

    Update selectivebackup.ps1

--- selectivebackup.ps1
+++ selectivebackup.ps1
@@ -2,6 +2,7 @@ $resourceGroupName="ai-cloud-poc"
 $recoveryServicesVaultName="vault329"
 $virtualMachineName="backuptest"
 $disks = ("0","1")
+$vault = Get-AzRecoveryServicesVault -ResourceGroupName $resourceGroupName -Name $recoveryServicesVaultName
 Set-AzRecoveryServicesVaultContext -Vault $vault
 $backupItem = Get-AzRecoveryServicesBackupItem -BackupManagementType "AzureVM" -WorkloadType "AzureVM" -VaultId $vault.ID | Where-Object {$_.Name -like "*$virtualMachineName*"}
 Enable-AzRecoveryServicesBackupProtection -Item $backupItem -InclusionDisksList $disks -VaultId $Vault.ID
